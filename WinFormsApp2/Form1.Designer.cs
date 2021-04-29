7e0d7c37ca197836cba0a387cf8ad98a117ca0fa
HassanAlgadah <h--san@hotmail.com>
2021-04-29 16:13:00 +0300

    init project

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Border Colors";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Fill";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Normal",
+            "Dash",
+            "Dot"});
+            this.comboBox1.Location = new System.Drawing.Point(2, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 33);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 551);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+
+
+    public partial class Form1 : Form
+    {
+        bool ismovebottom = false;
+        bool ismovetop = false;
+        bool ismatch = false;
+
+        Pen pen = new Pen(Brushes.White,3);
+        Pen penblue = new Pen(Color.Blue);
+        Pen penred = new Pen(Color.Red);
+        SolidBrush penbluefill = new SolidBrush(Color.Blue);
+        SolidBrush penredfill = new SolidBrush(Color.Red);
+        GraphicsPath GPbottom = new GraphicsPath();
+        GraphicsPath GPtop = new GraphicsPath();
+
+
+        // initial postions 
+        int xbot = 150;
+        int ybot = 150;
+        int xtop = 350;
+        int ytop = 150;
+        int width = 50;
+        int higth = 50;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            GPbottom.Reset();
+            GPtop.Reset();
+            Graphics g = e.Graphics;
+
+            RectangleF[] bottom = { new Rectangle(xbot, ybot, width, higth), new Rectangle(xbot, ybot+50, width, higth), new Rectangle(xbot+50, ybot+50, width, higth), new Rectangle(xbot+100, ybot+50, width, higth), new Rectangle(xbot+100, ybot, width, higth) };
+            RectangleF[] top = { new Rectangle(xtop, ytop, width, higth), new Rectangle(xtop+50, ytop, width, higth), new Rectangle(xtop+100, ytop, width, higth), new Rectangle(xtop+50, ytop+50, width, higth) };
+            GPbottom.AddRectangles(bottom);
+            GPtop.AddRectangles(top);
+            if (ismatch)
+            {
+                g.FillPath(penbluefill, GPtop);
+                g.FillPath(penredfill, GPbottom);
+            }
+            else
+            {
+                g.DrawPath(penblue, GPtop);
+                g.DrawPath(penred, GPbottom);
+            }
+        }
+
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (ismovebottom)
+            {
+                xbot = e.X;
+                ybot = e.Y;
+                Invalidate();
+            }
+            else if (ismovetop)
+            {
+                xtop = e.X;
+                ytop = e.Y;
+                Invalidate();
+            }
+
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (ismatch) ismatch = false;
+            if (GPbottom.GetBounds().Contains(e.Location))
+            {
+                ismovebottom = true;
+            }
+            else if (GPtop.GetBounds().Contains(e.Location))
+            {
+                ismovetop = true;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (ismovebottom || ismovetop)
+            {
+                if((ytop + 100 > ybot &&  ytop +100 < ybot+50) && (xtop+50 >= xbot+40 && xtop+100 <=xbot+120))
+                {
+                    ytop = ybot - 50;
+                    xtop = xbot ;
+                    ismatch = true;
+                }
+                Invalidate();
+            }
+
+            ismovetop = false;
+            ismovebottom = false;
+        }
+
+
+    }
+}
